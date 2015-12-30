7b2ec3c4b9ab6524600e7efbb4019776eb95b0d1
Adonis K <aklp08@gmail.com>
2015-12-30 03:14:47 +0200

    initial commit

---
+++ spec/atom-git-replay-view-spec.js
@@ -0,0 +1,9 @@
+var AtomGitReplayView;
+
+AtomGitReplayView = require('../lib/atom-git-replay-view');
+
+describe("AtomGitReplayView", function() {
+  return it("has one valid test", function() {
+    return expect("life").toBe("easy");
+  });
+});
