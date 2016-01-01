2d534e3f362adb12f9c98c08527262269e7625ab
Adonis K <aklp08@gmail.com>
2016-01-01 21:25:12 +0200

    no willpower to write specs now, PRs more than welcome

--- spec/atom-git-replay-view-spec.js
+++
@@ -1,9 +0,0 @@
-var AtomGitReplayView;
-
-AtomGitReplayView = require('../lib/atom-git-replay-view');
-
-describe("AtomGitReplayView", function() {
-  return it("has one valid test", function() {
-    return expect("life").toBe("easy");
-  });
-});
